d7359c68fde23449c5ab739a9ea897852a81209f
Mathieu Lecarme <mlecarme@bearstech.com>
2020-02-07 22:11:50 +0100

    More reader.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/fsnotify/fsnotify v1.4.7 // indirect
 	github.com/hpcloud/tail v1.0.0 // indirect
 	github.com/influxdata/tail v1.0.0
+	github.com/sirupsen/logrus v1.4.2
 	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
--- tail/tail.go
+++ tail/tail.go
@@ -1,13 +1,78 @@
 package tail
 
 import (
+	"encoding/json"
+	"errors"
+	"sync"
+
 	"github.com/influxdata/tail"
+	log "github.com/sirupsen/logrus"
 )
 
-type Tail struct {
-	tailer *tail.Tail
+type Router struct {
+	backends map[string]string // backend => project
+	projects map[string]chan *tail.Line
+	lock     sync.RWMutex
 }
 
-func New() *Tail {
-	return &Tail{}
+func (r *Router) project(backend string) (chan *tail.Line, bool) {
+	r.lock.RLock()
+	defer r.lock.Unlock()
+	p, ok := r.backends[backend]
+	if !ok { // backend unknown
+		log.Warn("Unknown backend : ", backend)
+		return nil, false
+	}
+	c, ok := r.projects[p]
+	if ok {
+		return c, true
+	}
+	log.Error("No project for backend ", backend)
+	return nil, false
+}
+
+func (r *Router) SetProjectBackend(project, backend string) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	_, ok := r.projects[project]
+	if !ok {
+		r.projects[project] = make(chan *tail.Line)
+	}
+	r.backends[backend] = project
+}
+
+func (r *Router) RemoveBackend(backend string) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	delete(r.backends, backend)
+	// remove orphans projects
+}
+
+func (r *Router) Read(path string) error {
+	t, err := tail.TailFile(path, tail.Config{Follow: true})
+	if err != nil {
+		return err
+	}
+	var blob map[string]interface{}
+	for line := range t.Lines {
+		err := json.Unmarshal([]byte(line.Text), &blob)
+		if err != nil {
+			log.WithError(err).Warn()
+		}
+		backendRaw, ok := blob["BackendName"]
+		if !ok {
+			err = errors.New("This log line hasn't BackendName key")
+			log.WithError(err).Warn()
+		}
+		backend, ok := backendRaw.(string)
+		if !ok {
+			err = errors.New("BackendName is not a string")
+			log.WithError(err).Warn()
+		}
+		reader, ok := r.project(backend)
+		if ok {
+			reader <- line
+		}
+	}
+	return nil
 }
